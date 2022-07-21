547e5dc9b622afa726f99c4374b37673304e4d45
Daniil <daniilhanchenkov@yandex.ru>
2022-07-21 05:55:26 +0300

    initial commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -20,13 +20,23 @@ import * as path from 'path';
         ConfigModule.forRoot({
             envFilePath: '.env'
         }),
+        // SequelizeModule.forRoot({
+        //     dialect: 'postgres',
+        //     host: process.env.POSTGRES_HOST,
+        //     port: Number(process.env.POSTGRES_PORT),
+        //     username: process.env.POSTGRES_USER,
+        //     password: process.env.POSTGRES_PASSWORD,
+        //     database: process.env.POSTGRES_DB,
+        //     models: [User, Post],
+        //     autoLoadModels: true
+        // }),
         SequelizeModule.forRoot({
             dialect: 'postgres',
-            host: process.env.POSTGRES_HOST,
-            port: Number(process.env.POSTGRES_PORT),
-            username: process.env.POSTGRES_USER,
-            password: process.env.POSTGRES_PASSWORD,
-            database: process.env.POSTGRES_DB,
+            host: 'ec2-54-87-179-4.compute-1.amazonaws.com',
+            port: 5432,
+            username: 'ibkveczyqtvhng',
+            password: 'd6eadfee7f844e65af54a6bf87fa23d5c64cdef301cea23774602ad0dd1f0c31',
+            database: 'd52c22i6in5mk8',
             models: [User, Post],
             autoLoadModels: true
         }),
